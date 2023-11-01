975d9c6219a3b9f438f8c2e5c6fee656392d884d
dmitto <dedbankai@gmail.com>
2023-11-01 15:21:05 -0500

    task_5.1c_passed

--- exercises/05_basic_scripts/answer_task_5_1c.py
+++ exercises/05_basic_scripts/answer_task_5_1c.py
@@ -51,7 +51,7 @@ london_co = {
 }
 
 device = input("Введите имя устройства: ")
-device_keys = list(london_co[device].keys())
-device_keys_str = ",".join(device_keys)
-param = input("Введите имя параметра ({}): ".format(device_keys_str))
-print(london_co[device].get(param, "Такого параметра нет"))
+params = ", ".join(london_co[device].keys())
+parameter = input(f"Введите имя параметра ({params}): ")
+
+print(london_co[device].get(parameter, "Такого параметра нет"))
