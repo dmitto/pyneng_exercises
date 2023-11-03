d4b80b90f34704e3abed9411c38c0be6d3b8af40
dmitto <dedbankai@gmail.com>
2023-11-03 14:24:23 -0500

    task_5.2a_passed

--- exercises/05_basic_scripts/answer_task_5_2a.py
+++ exercises/05_basic_scripts/answer_task_5_2a.py
@@ -25,16 +25,8 @@ Mask:
 255       255       255       0
 11111111  11111111  11111111  00000000
 
-
 Проверить работу скрипта на разных комбинациях хост/маска, например:
     10.0.5.195/28, 10.0.1.1/24
-
-Вывод сети и маски должен быть упорядочен также, как в примере:
-- столбцами
-- ширина столбца 10 символов (в двоичном формате
-  надо добавить два пробела между столбцами
-  для разделения октетов между собой)
-
 
 Подсказка:
 Есть адрес хоста в двоичном формате и маска сети 28. Адрес сети это первые 28 бит
@@ -50,28 +42,47 @@ bin_ip = "00001010000000010000000111000011"
 
 """
 
-ip_net = input("Введите IP-адрес сети в формате <адрес сети>/<маска>: ")
-ip_net_list = ip_net.split("/")
-ip_net_addr = ip_net_list[0].split(".")
-mask = int(ip_net_list[1])
-oct1, oct2, oct3, oct4 = [int(ip_net_addr[0]), int(ip_net_addr[1]), int(ip_net_addr[2]), int(ip_net_addr[3])]
-ip_net_mask_str = "1" * mask + "0" * (32 - mask)
-ip_addr_str = "{0:08b}{1:08b}{2:08b}{3:08b}".format(oct1, oct2, oct3, oct4)
-ip_addr_net = ip_addr_str[:mask] + "0" * (32 - mask)
-oct_n1, oct_n2, oct_n3, oct_n4 = [int(ip_addr_net[:8], 2), int(ip_addr_net[8:16], 2), int(ip_addr_net[16:24], 2), int(ip_addr_net[24:], 2)]
-ip_net_tmpl = """Network:
+network = input("Введите адрес сети: ")
+
+ip, mask = network.split("/")
+ip_list = ip.split(".")
+mask = int(mask)
+
+oct1, oct2, oct3, oct4 = [
+    int(ip_list[0]),
+    int(ip_list[1]),
+    int(ip_list[2]),
+    int(ip_list[3]),
+]
+bin_ip_str = "{:08b}{:08b}{:08b}{:08b}".format(oct1, oct2, oct3, oct4)
+bin_network_str = bin_ip_str[:mask] + "0" * (32 - mask)
+
+net1, net2, net3, net4 = [
+    int(bin_network_str[0:8], 2),
+    int(bin_network_str[8:16], 2),
+    int(bin_network_str[16:24], 2),
+    int(bin_network_str[24:32], 2),
+]
+
+bin_mask = "1" * mask + "0" * (32 - mask)
+m1, m2, m3, m4 = [
+    int(bin_mask[0:8], 2),
+    int(bin_mask[8:16], 2),
+    int(bin_mask[16:24], 2),
+    int(bin_mask[24:32], 2),
+]
+
+ip_output = """
+Network:
 {0:<8}  {1:<8}  {2:<8}  {3:<8}
 {0:08b}  {1:08b}  {2:08b}  {3:08b}"""
-print(ip_net_tmpl.format(oct_n1, oct_n2, oct_n3, oct_n4))
 
-oct_m1, oct_m2, oct_m3, oct_m4 = [int(ip_net_mask_str[:8], 2), int(ip_net_mask_str[8:16], 2), int(ip_net_mask_str[16:24], 2), int(ip_net_mask_str[24:], 2)]
-ip_net_mask_tmpl = """Mask:
+mask_output = """
+Mask:
 /{0}
 {1:<8}  {2:<8}  {3:<8}  {4:<8}
-{1:08b}  {2:08b}  {3:08b}  {4:08b}"""
-print(ip_net_mask_tmpl.format(mask, oct_m1, oct_m2, oct_m3, oct_m4))
+{1:08b}  {2:08b}  {3:08b}  {4:08b}
+"""
 
-
-
-
-
+print(ip_output.format(net1, net2, net3, net4))
+print(mask_output.format(mask, m1, m2, m3, m4))
