f378fc93df95939c7983854259f6315a4c102f07
dmitto <dedbankai@gmail.com>
2023-11-05 10:39:40 -0600

    task_6.2_passed

--- exercises/06_control_structures/answer_task_6_2.py
+++ exercises/06_control_structures/answer_task_6_2.py
@@ -12,15 +12,17 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
-ip = input("Введите ip-адрес: ")
-oct1 = int(ip.split(".")[0])
-if oct1 >=1 and oct1 <=223:
-    print("unicast")
-elif oct1 >= 224 and oct1 <= 239:
-    print("multicast")
-elif ip == "255.255.255.255":
+
+ip_address = input("введите ip-адрес: ")
+oct1 = int(ip_address.split(".")[0])
+
+if ip_address == "255.255.255.255":
     print("local broadcast")
-elif ip == "0.0.0.0":
+elif ip_address == "0.0.0.0":
     print("unassigned")
+elif 1 <= oct1 <= 223:
+    print("unicast")
+elif 224 <= oct1 <= 239:
+    print("multicast")
 else:
     print("unused")
