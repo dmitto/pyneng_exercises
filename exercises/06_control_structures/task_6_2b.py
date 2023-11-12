a4f0b102df953d4bc9c1ae6d2da3b5f72513b93d
dmitto <dedbankai@gmail.com>
2023-11-12 09:26:43 -0600

    task_6.2b_passed

--- exercises/06_control_structures/task_6_2b.py
+++ exercises/06_control_structures/task_6_2b.py
@@ -12,3 +12,33 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+ip_int = []
+ip_lst = []
+key = False
+while not key:
+    mk = 0
+    ip = input("Введите ip-адрес: ")
+    ip_list = ip.split(".")
+    if ip.count(".") == 3 and len(ip_list) == 4:
+        for ip_l in ip_list:
+            if ip_l.isdigit() and 0 <= int(ip_l) <= 255:
+                #ip_int.append(int(ip_l))
+                mk += 1
+    if mk != 4:
+        print("Неправильный IP-адрес")
+    else:
+        key = True
+
+#oct1 = ip_int[0]
+oct1 = int(ip_list[0])
+if 1 <= oct1 <=223:
+    print("unicast")
+elif 224 <= oct1 <= 239:
+    print("multicast")
+elif ip == "255.255.255.255":
+    print("local broadcast")
+elif ip == "0.0.0.0":
+    print("unassigned")
+else:
+    print("unused")
