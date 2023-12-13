9b254465c685d72828213549689867bca0c51010
dmitto <dedbankai@gmail.com>
2023-12-13 15:37:18 +0300

    task_15.4_passed

--- exercises/15_module_re/answer_task_15_4.py
+++ exercises/15_module_re/answer_task_15_4.py
@@ -5,11 +5,9 @@
 Создать функцию get_ints_without_description, которая ожидает как аргумент
 имя файла, в котором находится конфигурация устройства.
 
+
 Функция должна обрабатывать конфигурацию и возвращать список имен интерфейсов,
 на которых нет описания (команды description).
-
-Пример итогового списка:
-["Loopback0", "Tunnel0", "Ethernet0/1", "Ethernet0/3.100", "Ethernet1/0"]
 
 Пример интерфейса с описанием:
 interface Ethernet0/2
@@ -24,15 +22,28 @@ interface Loopback0
 
 Проверить работу функции на примере файла config_r1.txt.
 """
-
 import re
 
-def get_ints_without_description(config):
-    regex = re.compile("^interface (\S+).+?( description)?.+?!\n", re.S|re.M)
-    with open(config) as f:
-        match = regex.finditer(f.read())
-        result = [m.group(1) for m in match if not m.group(2)]
-    return result
 
-if __name__ == "__main__":
-    print(f"Результат работы функции:\n{get_ints_without_description('config_r1.txt')}")
+def get_ints_without_description(config):
+    regex = re.compile(r"!\ninterface (?P<intf>\S+)\n"
+                       r"(?P<descr> description \S+)?")
+    with open(config) as src:
+        match = regex.finditer(src.read())
+        result = [m.group('intf') for m in match if m.lastgroup == 'intf']
+        return result
+
+
+def get_ints_without_description(filename):
+    result_list = []
+    regex = r"^interface (?P<intf>\S+)|^ description (.+)\n"
+    with open(filename) as f:
+        for line in f:
+            match_line = re.search(regex, line)
+            if match_line:
+                if match_line.lastgroup == "intf":
+                    intf = match_line.group("intf")
+                    result_list.append(intf)
+                else:
+                    result_list.remove(intf)
+    return result_list
