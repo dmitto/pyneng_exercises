b9b59d24dc21b577172b9c54f4dfbd9df7be025e
dmitto <dedbankai@gmail.com>
2024-01-10 13:40:19 -0600

    task_17.3b_passed

--- exercises/17_serialization/answer_task_17_3b.py
+++ exercises/17_serialization/answer_task_17_3b.py
@@ -43,27 +43,23 @@
 > pip install graphviz
 
 """
-
 import yaml
-from pprint import pprint
 from draw_network_graph import draw_topology
 
 
-def transform_topology(topology_file):
-    data = {}
-    with open(topology_file) as f_in:
-        topology = yaml.safe_load(f_in)
-    for key, value in topology.items():
-        for key_1, value_1 in value.items():
-            data[(key, key_1)] = list(value_1.items())[0]
-    new_data = {}
-    for key, value in data.items():
-        k = new_data.get(value)
-        if k == None:
-            new_data[key] = value
-    return new_data
+def transform_topology(topology_filename):
+    with open(topology_filename) as f:
+        raw_topology = yaml.safe_load(f)
+
+    formatted_topology = {}
+    for l_device, peer in raw_topology.items():
+        for l_int, remote in peer.items():
+            r_device, r_int = list(remote.items())[0]
+            if not (r_device, r_int) in formatted_topology:
+                formatted_topology[(l_device, l_int)] = (r_device, r_int)
+    return formatted_topology
 
 
-if __name__ == '__main__':
-    scheme = transform_topology("topology.yaml")
-    draw_topology(scheme)
+if __name__ == "__main__":
+    formatted_topology = transform_topology("topology.yaml")
+    draw_topology(formatted_topology)
