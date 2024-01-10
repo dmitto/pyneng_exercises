c595458938933dda7e5b428d5c585309cc7cb789
dmitto <dedbankai@gmail.com>
2024-01-10 11:17:01 +0300

    task_17.3_passed

--- exercises/17_serialization/answer_task_17_3.py
+++ exercises/17_serialization/answer_task_17_3.py
@@ -25,20 +25,22 @@ R6           Fa 0/2          143           R S I           2811       Fa 0/0
 Проверить работу функции на содержимом файла sh_cdp_n_sw1.txt
 """
 import re
-from pprint import pprint
 
-def parse_sh_cdp_neighbors(content):
-    result = {}
-    regexp = r"(?P<neighbor_name>\w+) +(?P<intf_local>\S+ \S+) +\d+.*?\d+ +(?P<intf_neighbor>\S+ \S+)"
-    device_name = re.search("(\w+)\>", content).group(1)
-    result[device_name] = {}
-    match_iter = re.finditer(regexp, content)
-    for line in match_iter:
-        neigh, intf_l, intf_neigh = line.groups()
-        result[device_name][intf_l] = {neigh: intf_neigh}
-    return result
-   
 
-if __name__ == '__main__':
-    with open('sh_cdp_n_sw1.txt') as f:
-        pprint(parse_sh_cdp_neighbors(f.read()))
+def parse_sh_cdp_neighbors(command_output):
+    regex = re.compile(
+        r"(?P<r_dev>\w+)  +(?P<l_intf>\S+ \S+)"
+        r"  +\d+  +[\w ]+  +\S+ +(?P<r_intf>\S+ \S+)"
+    )
+    connect_dict = {}
+    l_dev = re.search(r"(\S+)[>#]", command_output).group(1)
+    connect_dict[l_dev] = {}
+    for match in regex.finditer(command_output):
+        r_dev, l_intf, r_intf = match.group("r_dev", "l_intf", "r_intf")
+        connect_dict[l_dev][l_intf] = {r_dev: r_intf}
+    return connect_dict
+
+
+if __name__ == "__main__":
+    with open("sh_cdp_n_sw1.txt") as f:
+        print(parse_sh_cdp_neighbors(f.read()))
