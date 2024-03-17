5981ea3980f5f1658c83c67ecf407e4bab2a4973
dmitto <dedbankai@gmail.com>
2024-03-17 12:53:35 -0500

    task_19.2_passed

--- exercises/19_concurrent_connections/answer_task_19_2.py
+++ exercises/19_concurrent_connections/answer_task_19_2.py
@@ -34,29 +34,31 @@ Ethernet0/1                unassigned      YES NVRAM  administratively down down
 
 Проверить работу функции на устройствах из файла devices.yaml
 """
-
-
 from itertools import repeat
 from concurrent.futures import ThreadPoolExecutor
+
 from netmiko import ConnectHandler
 import yaml
 
-def send_command_to_device(device, command):
+
+def send_show_command(device, command):
     with ConnectHandler(**device) as ssh:
         ssh.enable()
-        prompt = ssh.find_prompt()+command
         result = ssh.send_command(command)
-    return f"{prompt}\n{result}\n"
+        prompt = ssh.find_prompt()
+    return f"{prompt}{command}\n{result}\n"
+
 
 def send_show_command_to_devices(devices, command, filename, limit=3):
-    with open(filename, 'w') as file:
-        with ThreadPoolExecutor(max_workers=limit) as executor:
-            result = executor.map(send_command_to_device, devices, repeat(command))
-            for out in result:
-                file.write(out)
+    with ThreadPoolExecutor(max_workers=limit) as executor:
+        results = executor.map(send_show_command, devices, repeat(command))
+        with open(filename, "w") as f:
+            for output in results:
+                f.write(output)
 
 
-if __name__ == '__main__':
-    with open('devices.yaml') as f:
+if __name__ == "__main__":
+    command = "sh ip int br"
+    with open("devices.yaml") as f:
         devices = yaml.safe_load(f)
-    send_show_command_to_devices(devices, 'sh ip int br', 'out_19_2')
+    send_show_command_to_devices(devices, command, "result.txt")
